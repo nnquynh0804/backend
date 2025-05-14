c89571e0d9388e615649f974a80099a4ea134269
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-05-14 10:27:42 +0700

    Update api.js

--- mintshop-backend/api.js
+++ mintshop-backend/api.js
@@ -26,7 +26,7 @@ app.post("/api/token_generate", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
