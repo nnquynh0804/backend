03f82e60c230437f58fe450e6a597446eb7345e3
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-05-16 22:39:21 +0700

    Create User.js

---
+++ mintshop-backend/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ['admin', 'customer'], default: 'customer' }
+});
+
+module.exports = mongoose.model('User', UserSchema);
