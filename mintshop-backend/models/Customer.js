1a6eff481d0d1e7faf1b75c25763c0bfd17afbbb
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-05-16 15:56:29 +0700

    Update Customer.js

--- mintshop-backend/models/Customer.js
+++ mintshop-backend/models/Customer.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
+const accountSchema = new mongoose.Schema({
+  accountNumber: { type: String, required: true },
+  balance: { type: Number, required: true }
+});
+
 const customerSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  address: { type: String },
+  dob: { type: Date },
+  hashedPassword: { type: String, required: true },
+  accounts: [accountSchema]
 });
 
 module.exports = mongoose.model('Customer', customerSchema);
