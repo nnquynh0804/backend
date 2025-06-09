9cb7380bca5287b1de96dc405a796f12a9601054
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-06-09 21:01:23 +0700

    Update api.js

--- mintshop-backend/api.js
+++ mintshop-backend/api.js
@@ -8,6 +8,7 @@ const productRoutes = require('./routes/products');
 const customerRoutes = require('./routes/customers');
 const authRoutes = require('./routes/auth');
 const orderRoutes = require('./routes/orders'); // ✅ Thêm dòng này
+const uploadRouter = require('./routes/upload');
 
 const app = express();
 
@@ -28,7 +29,7 @@ app.use('/products', productRoutes);
 app.use('/customers', customerRoutes);
 app.use('/auth', authRoutes);
 app.use('/orders', orderRoutes); // ✅ Thêm dòng này
-
+app.use('/upload', uploadRouter);
 // Server
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
