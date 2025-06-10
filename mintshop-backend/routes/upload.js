922eb78a4c5136dfa3dff724497f94234efb41c5
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-06-10 14:58:27 +0700

    Update upload.js

--- mintshop-backend/routes/upload.js
+++ mintshop-backend/routes/upload.js
@@ -1,7 +1,6 @@
 // routes/upload.js
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
 const { v4: uuidv4 } = require('uuid');
 
 router.post('/', async (req, res) => {
