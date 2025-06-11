6cf81cd6557293730e8175247a0b9371d1279f79
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-06-11 13:55:38 +0700

    Update Product.js

--- mintshop-backend/models/Product.js
+++ mintshop-backend/models/Product.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  about: { type: String, required: true },
-  imageUrl: { type: String, required: true }
+  about: { type: String, required: false },
+  imageUrl: { type: String, required: true },
+  quantity: {type: Int, required: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
